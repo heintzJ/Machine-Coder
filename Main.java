a881d62422ad2565e3214ba732438467f1e7a15c
heintzJ <96451646+heintzJ@users.noreply.github.com>
2023-02-14 20:43:40 -0500

    Added countInputs() method

--- Main.java
+++ Main.java
@@ -19,42 +19,48 @@ public class Main {
         int count = 0;
 
         while (scanner.hasNext()){
-            if (count == 0){
-                if (scanner.next().equals("ld")){
-                    op = opcode("ld");
-                    machine_code.add(op);
-                    op3 = Instruction_sets.load();
-                    machine_code.add(op3);
+            int numberOfInputs = countInputs(instruction);
+            System.out.println(numberOfInputs);
+            if (numberOfInputs == 3){
+                if (count == 0){
+                    if (scanner.next().equals("ld")){
+                        op = opcode("ld");
+                        machine_code.add(op);
+                        op3 = Instruction_sets.load();
+                        machine_code.add(op3);
+                    }
+                        
+                    else if (scanner.next().equals("st")){
+                        op = opcode("ld");
+                        op3 = Instruction_sets.store();
+                        machine_code.add(op);
+                        machine_code.add(op3);
+                    }
+                    count++;
                 }
-                    
-                else if (scanner.next().equals("st")){
-                    op = opcode("ld");
-                    op3 = Instruction_sets.store();
-                    machine_code.add(op);
-                    machine_code.add(op3);
+                if (count == 1){
+                    if(scanner.hasNextInt()){
+                        simm13 = scanner.nextInt();
+                        String initialResult = Integer.toBinaryString(simm13);
+                        String finalResult = String.format("%13s", initialResult).replaceAll(" ", "0");
+                        machine_code.add(finalResult);
+                    }
+                    count++;
                 }
-                count++;
-            }
-            if (count == 1){
-                if(scanner.hasNextInt()){
-                    simm13 = scanner.nextInt();
-                    String initialResult = Integer.toBinaryString(simm13);
-                    String finalResult = String.format("%13s", initialResult).replaceAll(" ", "0");
-                    machine_code.add(finalResult);
+                if (count == 2){
+                    int num = scanner.next().charAt(2);
+                    rd = Integer.toBinaryString(num);
+                    machine_code.add(1, rd);
+                    System.out.println(num);
+                    System.out.println(rd);
                 }
-                count++;
-            }
-            if (count == 2){
-                int num = scanner.next().charAt(2);
-                rd = Integer.toBinaryString(num);
-                machine_code.add(1, rd);
-                System.out.println(num);
-                System.out.println(rd);
             }
 
         JOptionPane.showMessageDialog(null, machine_code);
         
         System.out.println(instruction);
+
+        scanner.close();
     }
 }
 
@@ -67,10 +73,22 @@ public class Main {
         return toUse.substring(start.length()+1, toUse.length()-1);
     }
 
+    // Find the opcode segment of the machine code
     public static String opcode(String input){
         if (input.equals("ld") || input.equals("st")){
             return "11";
         }
         return "";
+    }
+
+    // Count how many instructions are in the input
+    public static int countInputs(String input){
+        Scanner scanner = new Scanner(input);
+        int inputs = 0;
+        while(scanner.hasNext()){
+            inputs++;
+        }
+        scanner.close();
+        return inputs;
     }
 }
