ac3c71cf88cb91b9e625e7473d7c6b9857783c2c
heintzJ <96451646+heintzJ@users.noreply.github.com>
2023-02-15 11:48:33 -0500

    ld and st should be working

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 import java.util.Collections;
 
 public class Main {
+    public static String i;
     public static void main(String[] args) throws FileNotFoundException{
         String line = JOptionPane.showInputDialog(null, "Enter");
         Scanner scanner = new Scanner(line);
@@ -18,7 +19,6 @@ public class Main {
         String rd;
         String op3;
         String rs1;
-        String i;
         int simm13;
         String zeros = "00000000";
         String rs2;
@@ -38,7 +38,7 @@ public class Main {
             String instruction = ARC_code.get(0);
             if (instruction.equals("ld") || instruction.equals("st")){
                 op = opcode("ld");
-                machine_code.add(op);   
+                machine_code.add(op);
             }
             // Extract key: value pairs from text file
             for (var entry: binaryValues.entrySet()){
@@ -60,21 +60,54 @@ public class Main {
                     int numberOfRegister = Integer.parseInt(registerNumber.substring(1, registerNumber.length()));
                     String initial_rs1 = Integer.toBinaryString(numberOfRegister);
                     rs1 = String.format("%5s", initial_rs1).replaceAll(" ", "0");
-                    machine_code.add(rs1);
                     machine_code.add(zeros);
+                    machine_code.add(rs1);
                     i = "0";
-                    machine_code.add(3, i);
+                    machine_code.add(2, i);
                 }
                 registerNumber = ARC_code.get(2);
                 int numberOfRegister = Integer.parseInt(registerNumber.substring(1, registerNumber.length()));
                 String initial_rd = Integer.toBinaryString(numberOfRegister);
                 rd = String.format("%5s", initial_rd).replaceAll(" ", "0");
                 rs1 = "00000";
-                i = "1";
                 machine_code.add(1, rd);
                 machine_code.add(2, rs1);
                 // Check if i is already set
-                if (machine_code.get(4) != "0"){
+                if (i != "0"){
+                    i = "1";
+                    machine_code.add(4, i);
+                }
+                // Fixing order
+                Collections.swap(machine_code, 2, 3);
+            }
+            if (instruction.equals("st")){
+                try { 
+                    simm13 = (Integer.parseInt(ARC_code.get(2)));
+                    String initialResult = Integer.toBinaryString(simm13);
+                    String finalResult = String.format("%13s", initialResult).replaceAll(" ", "0");
+                    machine_code.add(finalResult);
+                }
+                // if the second element is not an int, then it is a register
+                catch (NumberFormatException ex){
+                    registerNumber = ARC_code.get(2);
+                    int numberOfRegister = Integer.parseInt(registerNumber.substring(1, registerNumber.length()));
+                    String initial_rs1 = Integer.toBinaryString(numberOfRegister);
+                    rs1 = String.format("%5s", initial_rs1).replaceAll(" ", "0");
+                    machine_code.add(zeros);
+                    machine_code.add(rs1);
+                    i = "0";
+                    machine_code.add(2, i);
+                }
+                registerNumber = ARC_code.get(1);
+                int numberOfRegister = Integer.parseInt(registerNumber.substring(1, registerNumber.length()));
+                String initial_rd = Integer.toBinaryString(numberOfRegister);
+                rd = String.format("%5s", initial_rd).replaceAll(" ", "0");
+                rs1 = "00000";
+                machine_code.add(1, rd);
+                machine_code.add(2, rs1);
+                // Check if i is already set
+                if (i != "0"){
+                    i = "1";
                     machine_code.add(4, i);
                 }
                 // Fixing order
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Machine-Coder
+
+Takes an intruction is assembly and turns it into machine code.
