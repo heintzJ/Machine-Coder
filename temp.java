db2468f7c9a1067315b9bde1481115ff3966168b
heintzJ <96451646+heintzJ@users.noreply.github.com>
2023-02-14 20:07:09 -0500

    Create temp.java

---
+++ temp.java
@@ -0,0 +1,76 @@
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class temp {
+    public static void main(String[] args) {
+        String instruction = JOptionPane.showInputDialog(null, "Enter");
+        Scanner scanner = new Scanner(instruction);
+        ArrayList<String> machine_code = new ArrayList<String>();
+        String op;
+        String rd;
+        String op3;
+        String rs1;
+        String i;
+        int simm13;
+        String zeros;
+        String rs2;
+
+        int count = 0;
+
+        while (scanner.hasNext()){
+            if (count == 0){
+                if (scanner.next().equals("ld")){
+                    op = opcode("ld");
+                    machine_code.add(op);
+                    op3 = Instruction_sets.load();
+                    machine_code.add(op3);
+                }
+                    
+                else if (scanner.next().equals("st")){
+                    op = opcode("ld");
+                    op3 = Instruction_sets.store();
+                    machine_code.add(op);
+                    machine_code.add(op3);
+                }
+                count++;
+            }
+            if (count == 1){
+                if(scanner.hasNextInt()){
+                    simm13 = scanner.nextInt();
+                    String initialResult = Integer.toBinaryString(simm13);
+                    String finalResult = String.format("%13s", initialResult).replaceAll(" ", "0");
+                    machine_code.add(finalResult);
+                }
+                count++;
+            }
+            if (count == 2){
+                int num = scanner.next().charAt(2);
+                rd = Integer.toBinaryString(num);
+                machine_code.add(1, rd);
+                System.out.println(num);
+                System.out.println(rd);
+            }
+
+        JOptionPane.showMessageDialog(null, machine_code);
+        
+        System.out.println(instruction);
+    }
+}
+
+    /**
+     * @param toUse Specify which string to use
+     * @param start Where the concatenation should start
+     * @return A substring starting at start, until the of of toUse
+     */
+    public static String remainingDigits(String toUse, String start){
+        return toUse.substring(start.length()+1, toUse.length()-1);
+    }
+
+    public static String opcode(String input){
+        if (input.equals("ld") || input.equals("st")){
+            return "11";
+        }
+        return "";
+    }
+}
