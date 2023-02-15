112791b76f0c5c3359182777d5699f53898d74a1
heintzJ <96451646+heintzJ@users.noreply.github.com>
2023-02-15 12:52:13 -0500

    Combined "ld" and "st" into same block

--- Main.java
+++ Main.java
@@ -6,7 +6,17 @@ import java.util.Scanner;
 import java.util.Collections;
 
 public class Main {
+    // Instruction format variables
     public static String i;
+    public static String op;
+    public static String rd;
+    public static String op3;
+    public static String rs1;
+    public static int simm13;
+    public static final String zeros = "00000000";
+    public static String rs2;
+    public static String registerNumber;
+
     public static void main(String[] args) throws FileNotFoundException{
         String line = JOptionPane.showInputDialog(null, "Enter");
         Scanner scanner = new Scanner(line);
@@ -14,15 +24,6 @@ public class Main {
         ArrayList<String> machine_code = new ArrayList<String>();
         // List of instructions
         ArrayList<String> ARC_code = new ArrayList<String>();
-        // Instruction format variables
-        String op;
-        String rd;
-        String op3;
-        String rs1;
-        int simm13;
-        String zeros = "00000000";
-        String rs2;
-        String registerNumber;
 
         int numberOfInputs = 0;
 
@@ -47,16 +48,26 @@ public class Main {
                     machine_code.add(op3);
                 }
             }
-            if (instruction.equals("ld")){
+            // if (instruction.equals("ld")){
                 try { 
-                    simm13 = (Integer.parseInt(ARC_code.get(1)));
+                    if (instruction.equals("ld")){
+                        simm13 = Integer.parseInt(ARC_code.get(1));
+                    }
+                    else if (instruction.equals("st")){
+                        simm13 = Integer.parseInt(ARC_code.get(2));
+                    }
                     String initialResult = Integer.toBinaryString(simm13);
                     String finalResult = String.format("%13s", initialResult).replaceAll(" ", "0");
                     machine_code.add(finalResult);
                 }
                 // if the second element is not an int, then it is a register
                 catch (NumberFormatException ex){
-                    registerNumber = ARC_code.get(1);
+                    if (instruction.equals("ld")){
+                        registerNumber = ARC_code.get(1);
+                    }
+                    else if (instruction.equals("st")){
+                        registerNumber = ARC_code.get(2);
+                    }
                     int numberOfRegister = Integer.parseInt(registerNumber.substring(1, registerNumber.length()));
                     String initial_rs1 = Integer.toBinaryString(numberOfRegister);
                     rs1 = String.format("%5s", initial_rs1).replaceAll(" ", "0");
@@ -65,40 +76,12 @@ public class Main {
                     i = "0";
                     machine_code.add(2, i);
                 }
-                registerNumber = ARC_code.get(2);
-                int numberOfRegister = Integer.parseInt(registerNumber.substring(1, registerNumber.length()));
-                String initial_rd = Integer.toBinaryString(numberOfRegister);
-                rd = String.format("%5s", initial_rd).replaceAll(" ", "0");
-                rs1 = "00000";
-                machine_code.add(1, rd);
-                machine_code.add(2, rs1);
-                // Check if i is already set
-                if (i != "0"){
-                    i = "1";
-                    machine_code.add(4, i);
-                }
-                // Fixing order
-                Collections.swap(machine_code, 2, 3);
-            }
-            if (instruction.equals("st")){
-                try { 
-                    simm13 = (Integer.parseInt(ARC_code.get(2)));
-                    String initialResult = Integer.toBinaryString(simm13);
-                    String finalResult = String.format("%13s", initialResult).replaceAll(" ", "0");
-                    machine_code.add(finalResult);
-                }
-                // if the second element is not an int, then it is a register
-                catch (NumberFormatException ex){
+                if (instruction.equals("ld")){
                     registerNumber = ARC_code.get(2);
-                    int numberOfRegister = Integer.parseInt(registerNumber.substring(1, registerNumber.length()));
-                    String initial_rs1 = Integer.toBinaryString(numberOfRegister);
-                    rs1 = String.format("%5s", initial_rs1).replaceAll(" ", "0");
-                    machine_code.add(zeros);
-                    machine_code.add(rs1);
-                    i = "0";
-                    machine_code.add(2, i);
                 }
-                registerNumber = ARC_code.get(1);
+                else if (instruction.equals("st")){
+                    registerNumber = ARC_code.get(1);
+                }
                 int numberOfRegister = Integer.parseInt(registerNumber.substring(1, registerNumber.length()));
                 String initial_rd = Integer.toBinaryString(numberOfRegister);
                 rd = String.format("%5s", initial_rd).replaceAll(" ", "0");
@@ -109,7 +92,7 @@ public class Main {
                 if (i != "0"){
                     i = "1";
                     machine_code.add(4, i);
-                }
+                // }
                 // Fixing order
                 Collections.swap(machine_code, 2, 3);
             }
